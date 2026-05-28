182dba0414575c80948c7c2139b2abd1c4ce98bc
bddn-dbg <imi4172@duck.com>
2026-05-28 07:56:16 +0530

    Create hit_portals.py

---
+++ hit_portals.py
@@ -0,0 +1,33 @@
+import requests
+import random
+import time
+
+urls = [
+    "https://dynamic-portal-omega.vercel.app",
+    "https://dynamic-portal-bddn-web.netlify.app",
+    "https://demo-ecommerce-bddn.netlify.app",
+    "https://imran41.github.io/bihar-digital-data/",
+    "https://imran41.github.io/portfolio/",
+    "https://bddn-dbg.github.io/MistriBazaar/",
+    "https://mb-data-collection.vercel.app/",
+    "https://bddn-dbg.github.io/bddn/",
+    "https://bddn-dbg.github.io/fancydata/",
+    "https://merry-wisp-83f47c.netlify.app",
+    "https://bihar-digital-data-and-network-hnou.vercel.app/",
+    "https://imran41.github.io/bddn/"
+]
+
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+for url in urls:
+    try:
+        response = requests.get(url, headers=headers, timeout=20)
+
+        print(url, response.status_code)
+
+        time.sleep(random.randint(5, 15))
+
+    except Exception as e:
+        print(url, e)
