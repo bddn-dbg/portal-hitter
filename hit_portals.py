6a1a0f750b16ca94b9d7dfc26c9b99a4e20049ca
bddn-dbg <imi4172@duck.com>
2026-06-25 16:46:59 +0530

    Update hit_portals.py

--- hit_portals.py
+++ hit_portals.py
@@ -6,6 +6,8 @@ urls = [
     "https://bddn.online",
     "https://bddn.in",
     "https://clinquant-gecko-62db4d.netlify.app/",
+    "https://keen-malasada-29933c.netlify.app/",
+    
     "https://dynamic-portal-omega.vercel.app",
     "https://dynamic-portal-bddn-web.netlify.app",
     "https://demo-ecommerce-bddn.netlify.app",
